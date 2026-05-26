57fa5536a63d8f37d1e4b348f73e08720fab930f
Ismah Hassan <ismahiesha.hassan@gmail.com>
2026-05-25 19:09:07 -0500

    added new event feature and, event list shows capacity and remaining spots

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import './App.css'
 
 const Event = {
   name: "", 
-  capacity: 0
+  capacity: 0,
+  guests: []
 }
 
 function EventList({eventList}) {
   if(eventList.length === 0) return <p className="no-events">No events</p>
   
   return (
-      <ul className="eventlist">  
+      <ul className="event-list">  
         {eventList.map(event => 
-          <span className="events" key={event.name}>
-            {event.name}
-          </span>
+          <li className="events" key={event.name}>
+            {event.name} - {event.capacity} spots total • {event.capacity - event.guests.length} spots remaining
+          </li>
         )}
       </ul>
   )
@@ -22,11 +23,52 @@ function EventList({eventList}) {
 
 function App() {
   const [eventList, setEventList] = useState([])
-  
+  const [capacity, setCapacity] = useState("")
+  const [event, setEvent] = useState("")
+
+  function addEvent() {
+    if(event.trim().length === 0) return
+
+    const newEvent = {
+      name: event,
+      capacity: Number(capacity), //change to number instead
+      guests: []
+    }
+    
+    setEventList([...eventList, newEvent])
+    
+    setEvent("")
+    setCapacity("")
+  }
+
   return (
     <div>
-      <h1>Events</h1>
-      <EventList eventList={eventList}/>
+      <div className="create-event">
+        <h3>Create New Event</h3>
+
+        <input
+          type="text"
+          value={event}
+          placeholder="Event name"
+          onChange={e => setEvent(e.target.value)}
+        />
+
+        <input
+          type="text"
+          value={capacity}
+          placeholder="Max capacity"
+          onChange={e => setCapacity(e.target.value)}
+        />
+
+        <button className="add-button" onClick={addEvent}>
+          Add Event
+        </button>
+      </div>
+      
+      <div className="events-grid">
+        <EventList eventList={eventList}/>
+      </div>
+
     </div>
   )
 }
